161a106c17e745e1d78ebde316e567cc536be62c
JustinBaldock <github@baldock.net>
2023-12-13 21:16:58 +1030

    works to a point, not as nice as i want

--- 2023-12-Tables/table-demo.asm
+++ 2023-12-Tables/table-demo.asm
@@ -20,10 +20,12 @@ jmp start
 
 .include "kernal.inc"
 .include "variables.inc"
+.include "petsciiCodes.inc"
 
 
 sine:   .byte 128,176,217,245,255,254,245,217,175,128,77,36,8,0,8,36,78
-screenX:.byte 10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170
+;screenX:.byte 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
+
 
 ; ----------
 ; start program
@@ -33,20 +35,24 @@ start:
     ldx #0
     stx SINE_POINT
     ; switch to graphic screen mode, set size to zero, which is full screen 
-    stz KERNAL_R0_L
-    stz KERNAL_R0_H
-    stz KERNAL_R1_L
-    stz KERNAL_R1_H
-    stz KERNAL_R2_L
-    stz KERNAL_R2_H
-    stz KERNAL_R3_L
-    stz KERNAL_R3_H
-    jsr KERNEL_GRAPH_INIT
-    jsr KERNAL_GRAPH_SET_WINDOW
-    jsr KERNAL_GRAPH_CLEAR
+    ;stz KERNAL_R0_L
+    ;stz KERNAL_R0_H
+    ;stz KERNAL_R1_L
+    ;stz KERNAL_R1_H
+    ;stz KERNAL_R2_L
+    ;stz KERNAL_R2_H
+    ;stz KERNAL_R3_L
+    ;stz KERNAL_R3_H
+    ;jsr KERNEL_GRAPH_INIT
+    ;jsr KERNAL_GRAPH_SET_WINDOW
+    ;jsr KERNAL_GRAPH_CLEAR
     ; set the graphic pen color
-    lda #3
-    jsr KERNAL_GRAPH_SET_COLORS
+    ;lda #3
+    ;jsr KERNAL_GRAPH_SET_COLORS
+
+    ; clear the screen
+    lda #CTRL_CLR
+    jsr KERNAL_CHROUT
 
     jmp main
 
@@ -77,17 +83,27 @@ main:
 ; function - lookup table sine wave
 ; ----------
 printSine:
-    ldx SINE_POINT ; get the current table pointer
-    lda sine,x  ; get the current sine value from the lookup table
-    ldy screenX,x   ; get the current screen x value from the lookup table
+    ldy SINE_POINT ; get the current table pointer into y
+    sty SCREEN_X ; set the current pointer value to be screen x
+    ldx sine,y  ; get the current sine value from the lookup table
+    txa ; move x to a
+    lsr ; logical shift right
+    lsr
+    lsr
+    sta SCREEN_Y
+    
     ; draw a 'line' dot
-    sty KERNAL_R0 ; x   
-    sta KERNAL_R1 ; y
-    sty KERNAL_R2 ; same x so its a dot
-    sta KERNAL_R3 ; same y so its a dot
-    jsr KERNAL_GRAPH_DRAW_LINE
-
-    ; increse the pointer
+    ;sty KERNAL_R0 ; x   
+    ;sta KERNAL_R1 ; y
+    ;sty KERNAL_R2 ; same x so its a dot
+    ;sta KERNAL_R3 ; same y so its a dot
+    ;jsr KERNAL_GRAPH_DRAW_LINE
+    ldx SCREEN_Y
+    ldy SCREEN_X
+    jsr KERNAL_PLOT
+    lda #CHAR_ZERO
+    jsr KERNAL_CHROUT
+    ; increase the pointer
     inc SINE_POINT
     lda SINE_POINT
     cmp #17
